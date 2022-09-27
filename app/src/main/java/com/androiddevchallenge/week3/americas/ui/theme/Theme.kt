68002ba131df3d457615f0c8cb8e7b2aacb143a1
David Mendoza <david.mendoza.mnez@gmail.com>
2022-09-27 21:41:44 +0200

    Define color palettes for light and dark themes
    Add project colors and define color palettes for light and dark themes.

--- app/src/main/java/com/androiddevchallenge/week3/americas/ui/theme/Theme.kt
+++ app/src/main/java/com/androiddevchallenge/week3/americas/ui/theme/Theme.kt
@@ -21,25 +21,22 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
+private val LightColorPalette = lightColors(
+    primary = Yellow,
+    background = Purple,
+    surface = White,
+    onPrimary = Gray900,
+    onBackground = White,
+    onSurface = Gray900
 )
 
-private val LightColorPalette = lightColors(
-    primary = purple500,
-    primaryVariant = purple700,
-    secondary = teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkColorPalette = darkColors(
+    primary = Yellow,
+    background = Gray900,
+    surface = Gray700,
+    onPrimary = Gray900,
+    onBackground = White,
+    onSurface = White
 )
 
 @Composable

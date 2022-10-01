74e768c6cc2d9885ff44773e85a77e3cda876da8
David Mendoza <david.mendoza.mnez@gmail.com>
2022-10-01 21:01:06 +0200

    Create the Welcome screen
    Create the Welcome file.
    Define the Welcome composable.
    Add the Welcome composable to the NavHost.

--- app/src/main/java/com/androiddevchallenge/week3/americas/ui/NavGraph.kt
+++ app/src/main/java/com/androiddevchallenge/week3/americas/ui/NavGraph.kt
@@ -20,6 +20,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.androiddevchallenge.week3.americas.ui.screen.welcome.Welcome
 
 /**
  * Destinations used in the [WeTradeApp].
@@ -39,7 +40,7 @@ fun NavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(Destinations.WELCOME.route) {}
+        composable(Destinations.WELCOME.route) { Welcome() }
         composable(Destinations.LOG_IN.route) {}
         composable(Destinations.HOME.route) {}
     }

1432e45e8acd0eb7e3d03deb382747a57c0c7347
David Mendoza <david.mendoza.mnez@gmail.com>
2022-10-07 20:51:22 +0200

    Add the components to the Welcome screen
    Add the detailed components in the design to the Welcome screen.

--- app/src/main/java/com/androiddevchallenge/week3/americas/ui/screen/welcome/Welcome.kt
+++ app/src/main/java/com/androiddevchallenge/week3/americas/ui/screen/welcome/Welcome.kt
@@ -17,12 +17,88 @@ package com.androiddevchallenge.week3.americas.ui.screen.welcome
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androiddevchallenge.week3.americas.R
 import com.androiddevchallenge.week3.americas.ui.theme.WeTradeTheme
 
 @Composable
 fun Welcome() {
+
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.bg_welcome),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.logotype),
+            contentDescription = stringResource(id = R.string.app_name),
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(start = 16.dp, end = 16.dp, bottom = 32.dp),
+            horizontalArrangement = Arrangement.spacedBy(
+                space = 8.dp, alignment = Alignment.CenterHorizontally
+            )
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .weight(1f)
+                    .height(48.dp),
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.primary
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_sign_up),
+                    color = MaterialTheme.colors.onPrimary,
+                    style = MaterialTheme.typography.button
+                )
+            }
+
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier
+                    .weight(1f)
+                    .height(48.dp),
+                shape = MaterialTheme.shapes.large
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_log_in),
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.button
+                )
+            }
+        }
+    }
 }
 
 @Preview(name = "Light Theme", widthDp = 360, heightDp = 640, uiMode = UI_MODE_NIGHT_NO)

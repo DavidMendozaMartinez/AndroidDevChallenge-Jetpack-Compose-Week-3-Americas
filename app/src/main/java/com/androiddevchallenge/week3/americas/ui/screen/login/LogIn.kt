3a9ace24a803697955327a038464e9d3909e133b
David Mendoza <david.mendoza.mnez@gmail.com>
2022-10-25 09:20:02 +0200

    Add the components to the Log in screen

--- app/src/main/java/com/androiddevchallenge/week3/americas/ui/screen/login/LogIn.kt
+++ app/src/main/java/com/androiddevchallenge/week3/americas/ui/screen/login/LogIn.kt
@@ -16,16 +16,157 @@
 package com.androiddevchallenge.week3.americas.ui.screen.login
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Password
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androiddevchallenge.week3.americas.R
+import com.androiddevchallenge.week3.americas.ui.theme.Gray900
 import com.androiddevchallenge.week3.americas.ui.theme.WeTradeTheme
+import com.androiddevchallenge.week3.americas.ui.theme.White
 
 @Composable
 fun LogIn() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val backgroundColor: Color = if (isSystemInDarkTheme()) Gray900 else White
+
+    Column(modifier = Modifier.background(color = backgroundColor)) {
+        Box(contentAlignment = Alignment.TopCenter) {
+            Image(
+                painter = painterResource(id = R.drawable.bg_log_in),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillBounds
+            )
+
+            Text(
+                text = stringResource(id = R.string.log_in_title),
+                modifier = Modifier.paddingFromBaseline(top = 152.dp),
+                color = MaterialTheme.colors.onBackground,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h2
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .height(56.dp),
+            label = {
+                Text(
+                    text = stringResource(id = R.string.log_in_label_email),
+                    color = MaterialTheme.colors.onSurface,
+                    style = MaterialTheme.typography.body1
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.MailOutline,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            singleLine = true,
+            shape = MaterialTheme.shapes.small
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .height(56.dp),
+            label = {
+                Text(
+                    text = stringResource(id = R.string.log_in_label_password),
+                    color = MaterialTheme.colors.onSurface,
+                    style = MaterialTheme.typography.body1
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Password,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true,
+            shape = MaterialTheme.shapes.small
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = MaterialTheme.shapes.large,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.log_in_button_log_in),
+                color = MaterialTheme.colors.onPrimary,
+                style = MaterialTheme.typography.button
+            )
+        }
+    }
 }
 
-@Preview(name = "Light Theme", widthDp = 360, heightDp = 640, uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Preview(name = "Dark Theme", widthDp = 360, heightDp = 640, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(
+    name = "Light Theme", widthDp = 360, heightDp = 640, uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Dark Theme", widthDp = 360, heightDp = 640, uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
 fun LogInPreview() {
     WeTradeTheme {

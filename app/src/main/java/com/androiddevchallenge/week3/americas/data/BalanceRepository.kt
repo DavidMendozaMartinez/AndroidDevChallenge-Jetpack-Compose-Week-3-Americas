65c4fba09947d056f35fdde325acfd719fd6904b
David Mendoza <david.mendoza.mnez@gmail.com>
2023-04-23 20:27:58 +0200

    Update BalanceRepository.kt

--- app/src/main/java/com/androiddevchallenge/week3/americas/data/BalanceRepository.kt
+++ app/src/main/java/com/androiddevchallenge/week3/americas/data/BalanceRepository.kt
@@ -32,37 +32,5 @@ object BalanceRepositoy {
             total = BigDecimal(8_983),
             change = 8.568F
         ),
-        Balance(
-            total = BigDecimal(283),
-            change = 1.68F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
-        Balance(
-            total = BigDecimal(8_983),
-            change = 8.568F
-        ),
     )
 }

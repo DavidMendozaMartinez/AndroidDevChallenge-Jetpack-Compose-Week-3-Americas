eae9463fe25b5339f36d55efa0f237677b5f1c4e
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2025-07-31 13:51:02 +0200

    Create logotype and button components
    Create Logotype component.
    Create ThemedButton component.
    Create ThemedOutlinedButton component.

---
+++ app/src/main/java/com/androiddevchallenge/week3/americas/ui/component/theme/ThemedButton.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.androiddevchallenge.week3.americas.ui.component.theme
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.androiddevchallenge.week3.americas.ui.theme.WeTradeTheme
+
+@Composable
+fun ThemedButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.defaultMinSize(minHeight = MinHeight),
+        enabled = enabled,
+        interactionSource = interactionSource,
+        elevation = null,
+        shape = MaterialTheme.shapes.large,
+        border = null,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primary,
+            contentColor = MaterialTheme.colors.onPrimary,
+            disabledBackgroundColor = Color.Unspecified,
+            disabledContentColor = Color.Unspecified,
+        ),
+        contentPadding = contentPadding,
+    ) {
+        Text(
+            text = text.uppercase(),
+            style = MaterialTheme.typography.button,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ThemedButtonPreview() {
+    WeTradeTheme {
+        ThemedButton(
+            text = LoremIpsum(words = 3).values.first(),
+            onClick = {},
+        )
+    }
+}
+
+private val MinHeight: Dp = 48.dp
